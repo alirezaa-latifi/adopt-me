24f188b60056a77e22e947c91e4abdae7676b313
Alireza Latifi <l.alirezalatifi@gmail.com>
2022-12-13 14:20:03 +0330

    Fix api urls

--- src/APIs/fetchBreeds.js
+++ src/APIs/fetchBreeds.js
@@ -2,7 +2,7 @@ async function fetchBreeds({ queryKey }) {
   const animal = queryKey[1];
   if (!animal) return [];
   const response = await fetch(
-    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+    `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
   );
   if (!response.ok) throw new Error(`breeds ${animal} fetch not ok`);
   return response.json();
--- src/APIs/fetchDetail.js
+++ src/APIs/fetchDetail.js
@@ -1,6 +1,6 @@
 async function fetchDetail({ queryKey }) {
   const id = queryKey[1];
-  const response = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+  const response = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
 
   if (!response.ok) throw new Error(`details/${id} fetch not ok`);
 
