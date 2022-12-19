c7ffce729dcb5fc31db94ae63e60ee2ca69adc39
alirezaa-latifi <l.alirezalatifi@gmail.com>
2022-12-19 06:16:04 -0800

    Add test: Display none-default thumbnail

--- src/__tests__/Pet.test.js
+++ src/__tests__/Pet.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import { StaticRouter } from "react-router-dom/server";
 import Pet from "../Components/Pet";
 
-test("Displays default thumbnails", async () => {
+test("Displays default thumbnail", async () => {
   const pet = render(
     <StaticRouter>
       <Pet />
@@ -12,4 +12,15 @@ test("Displays default thumbnails", async () => {
 
   const petThumbnail = await pet.findByTestId("thumbnail");
   expect(petThumbnail.src).toContain("none.jpg");
+});
+
+test("Display none-default thumbnail", async () => {
+  const pet = render(
+    <StaticRouter>
+      <Pet images={["1.jpg", "2.jpg"]} />
+    </StaticRouter>
+  );
+
+  const petThumbnail = await pet.findByTestId("thumbnail");
+  expect(petThumbnail.src).toContain("1.jpg");
 });
